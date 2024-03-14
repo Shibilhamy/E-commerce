aad8b848dd20ae0a99144b8ab0972bdb8e3c05dd
shibilhamy <shibilmonot@gmail.com>
2024-03-14 11:32:32 +0530

    first commit

---
+++ app1/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from . models import *
+admin.site.register(ProductModel)
+admin.site.register(CategoryModel)
+admin.site.register(AvailableOffers)
+admin.site.register(GenderModel)
+admin.site.register(ProfileModel)
+admin.site.register(CartItem)
+admin.site.register(InventoryModel)
+# Register your models here.
